7756ed9dcc4b7a620381aad4c16a5ee87eb36493
fen <guangyaol@olymtech.com>
2017-10-18 13:23:46 +0800

    数字格式化

--- src/util/index.js
+++ src/util/index.js
@@ -23,15 +23,38 @@ const date = (val, opt = 'default') => {
 
     // 以后可以穿 object 进来， object.format 就是格式名称
     const formatType = typeof opt === 'string' ? opt : opt.format
-    
     const format = formats[formatType] || formatType
-
     return moment(val).format(format)
 }
 
+// 数字格式化
+const numeric = (value, decimals) => {
+    var digitsRE = /(\d{3})(?=\d)/g
+    value = parseFloat(value)
+    if (!isFinite(value) || (!value && value !== 0)) return ''
+    // currency = currency != null ? currency : '$'
+    decimals = decimals != null ? decimals : 2
+    var stringified = Math.abs(value).toFixed(decimals)
+    console.log(stringified.slice(0, -1 - decimals))
+    
+    var _int = stringified.slice(0, -1 - decimals)
+    var i = _int.length % 3
+    var head = i > 0
+      ? (_int.slice(0, i) + (_int.length > 3 ? ',' : ''))
+      : ''
+    var _float = decimals
+      ? stringified.slice(-1 - decimals)
+      : ''
+    var sign = value < 0 ? '-' : ''
+    return sign + head +
+      _int.slice(i).replace(digitsRE, '$1,') +
+      _float
+  }
+
 const util = {
     moment,
-    date
+    date,
+    numeric
 }
 
 const mergedUtil = Object.assign(util, lodash)
