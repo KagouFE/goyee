6bfcac32bbd0725d47ec047d7fa888c21bc4528f
fen <guangyaol@olymtech.com>
2017-10-18 13:22:58 +0800

    数字格式化

--- src/filters/index.js
+++ src/filters/index.js
@@ -1,9 +1,11 @@
 import uppercase from './uppercase'
 import sum from './sum'
 import date from './date'
+import numeric from './numeric'
 
 export default {
     uppercase,
     sum,
-    date
+    date,
+    numeric
 }
